b09d436166f0b858337c31136b82fbae1ac07990
Jayanth Koushik <jnkoushik@gmail.com>
2018-03-25 04:31:34 -0400

    Fix latex issues

--- _plugins/jekyll-pandoc.rb
+++ _plugins/jekyll-pandoc.rb
@@ -9,10 +9,12 @@ class Jekyll::Converters::Markdown::PandocProcessor
         @converter = PandocRuby.new(content,
                                     :from => :"markdown-markdown_in_html_blocks")
         @converter.to_html(:katex,
-                           :bibliography => "_includes/references.bib",
+                           :bibliography => :"_includes/references.bib",
                            :F => :"pandoc-crossref",
                            :filter => :"pandoc-citeproc",
+                           :csl => :"_includes/references.csl",
                            :M =>    :"linkReferences=true\
+                                   -M link-citations=true\
                                    -M figPrefix=Figure\
                                    -M eqnPrefix=Equation\
                                    -M tblPrefix=Table\
