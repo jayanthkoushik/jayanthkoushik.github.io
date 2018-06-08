85e8b4def6621464ad4d3fc7a98a050ab5ee29cd
Jayanth Koushik <jnkoushik@gmail.com>
2018-06-07 22:09:21 -0400

    Remove google analytics

--- js/custom.js
+++ js/custom.js
@@ -25,12 +25,3 @@ $(document).ready(function () {
     $("figure").attr("style", "padding-top: 20px");
     $("figcaption").attr("style", "padding-top: 20px");
 });
-
-(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-ga('create', 'UA-49245567-2', 'auto');
-ga('send', 'pageview');
