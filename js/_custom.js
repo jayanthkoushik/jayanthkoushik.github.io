a63af0766bc0c5b263fcd7413dd6277076d23c41
Jayanth Koushik <jnkoushik@gmail.com>
2019-10-23 02:54:09 -0400

    Implement style without javascript

--- js/_custom.js
+++ js/_custom.js
@@ -4,26 +4,3 @@ $(document).on('click', '[data-toggle="lightbox"]', function (event) {
 });
 
 renderMathInElement(document.body);
-
-$(document).ready(function () {
-    $("#refs").prepend("<h1>References</h1>")
-
-    $("h5").replaceWith(function() { return $("<h6>", {"id": this.id, "html": $(this).html()}); });
-    $("h4").replaceWith(function() { return $("<h5>", {"id": this.id, "html": $(this).html()}); });
-    $("h3").replaceWith(function() { return $("<h4>", {"id": this.id, "html": $(this).html()}); });
-    $("h2").replaceWith(function() { return $("<h3>", {"id": this.id, "html": $(this).html()}); });
-    $("h1").replaceWith(function() { return $("<h2>", {"id": this.id, "html": $(this).html()}); });
-    $("#title").replaceWith(function() { return $("<h1>", {"id": this.id, "html": $(this).html()}); });
-
-    $("h2").before("<hr>");
-
-    $("table").addClass("table");
-    $("thead").addClass("thead-light");
-
-    $("figure").addClass("text-center");
-    $("figure img").addClass("img-fluid");
-    $("figure").attr("style", "padding-top: 20px");
-    $("figcaption").attr("style", "padding-top: 20px");
-
-    $(".header-section-number").append(".").replaceWith(function() { return $("<small>", {"html": $(this).html()}); });
-});
