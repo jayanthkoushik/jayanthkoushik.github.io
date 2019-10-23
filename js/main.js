8308112a196b9c94f3235c3cca5d130a61bd6846
Jayanth Koushik <jnkoushik@gmail.com>
2019-10-23 02:54:54 -0400

    Update bootstrap and jquery

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ permalink: assets/main
 {%- capture customjs -%}
   {%- include_relative _custom.js -%}
 {%- endcapture -%}
-{%- include_relative _jquery.min.js -%}
+{%- include_relative _jquery-slim.min.js -%}
 {%- include_relative _bootstrap.min.js -%}
 {%- include_relative _ekko-lightbox.min.js -%}
 {%- include_relative _katex.min.js -%}
